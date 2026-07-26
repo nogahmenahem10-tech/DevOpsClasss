a5d54c833a9e2467385e2909cf8c0b72f1dc5b76
“nogahmenahem10-tech <nogahmenahem10@gmail.com>
2026-07-26 19:01:00 +0300

    Initial Java files

---
+++ GITrepo-ex3-NogaMenahem/Noga.java
@@ -0,0 +1,7 @@
+public class Noga {
+    public static void main(String[] args) {
+        System.out.println("Noga");
+        System.out.println("Noga");
+        System.out.println("Noga");
+    }
+}
